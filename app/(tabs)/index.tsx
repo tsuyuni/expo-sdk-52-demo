8df00899aaf11c665fd5784539b2fe53dff08dc0
tsuyuni <tsuyu.lit@gmail.com>
2024-12-20 03:03:44 +0900

    [add] expo-audio

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,73 +1,31 @@
-import { useEvent } from "expo";
-import {
-  isPictureInPictureSupported,
-  useVideoPlayer,
-  VideoSource,
-  VideoView,
-} from "expo-video";
-import { StyleSheet, View, Button } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
+import { useAudioPlayer } from "expo-audio";
 
-const videoSource: VideoSource = {
-  uri: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  metadata: {
-    title: "Big Buck Bunny",
-    artwork:
-      "https://i.vimeocdn.com/video/20963649-f02817456fc48e7c317ef4c07ba259cd4b40a3649bd8eb50a4418b59ec3f5af5-d?mw=3000&mh=1245&q=70",
-  },
-};
-
-const VideoScreen = () => {
-  const player = useVideoPlayer(videoSource, (player) => {
-    player.showNowPlayingNotification = true;
-    player.loop = true;
-    player.play();
-  });
-
-  const { isPlaying } = useEvent(player, "playingChange", {
-    isPlaying: player.playing,
+const App = () => {
+  const player = useAudioPlayer({
+    uri: "https://github.com/tsuyuni/expo-sdk-52-demo/blob/main/assets/songs/maou_inst_14_shining_star.mp3",
   });
 
   return (
-    <View style={styles.contentContainer}>
-      <VideoView
-        style={styles.video}
-        player={player}
-        allowsFullscreen
-        allowsPictureInPicture
-        // startsPictureInPictureAutomatically
+    <View style={styles.container}>
+      <Button
+        title="Play Sound"
+        onPress={() => {
+          player.play();
+          console.log(player);
+        }}
       />
-      <View style={styles.controlsContainer}>
-        <Button
-          title={isPlaying ? "Pause" : "Play"}
-          onPress={() => {
-            console.log(isPictureInPictureSupported());
-            // if (isPlaying) {
-            //   player.pause();
-            // } else {
-            //   player.play();
-            // }
-          }}
-        />
-      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contentContainer: {
+  container: {
     flex: 1,
-    padding: 10,
-    alignItems: "center",
     justifyContent: "center",
-    paddingHorizontal: 50,
-  },
-  video: {
-    width: 350,
-    height: 275,
-  },
-  controlsContainer: {
+    backgroundColor: "#ecf0f1",
     padding: 10,
   },
 });
 
-export default VideoScreen;
+export default App;
